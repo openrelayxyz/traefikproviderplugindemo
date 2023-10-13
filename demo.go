3b4b407df8ca86b202fba1c314747f8383f990ba
Austin Roberts <austin.roberts@rivet.cloud>
2023-10-13 12:07:18 -0500

    Initial proof-of-concept implementation
    This is a work-in-progress proof-of-concept. It doesn't compile
    at the moment, but I wanted to get it out there.
    
    The basic idea behind this is to automatically set up the services,
    middlewares, and routes for routing RPC traffic to a range of different
    providers, with block number informing the determination of the service's
    health. At the moment, the block number subscription isn't working as
    intended because gorilla websockets doesn't work with Yaegi. I'm working
    on rewriting for a different websockets library, but wanted to make
    current state available.

--- demo.go
+++ demo.go
@@ -7,50 +7,131 @@ import (
 	"fmt"
 	"log"
 	"time"
+	// "net/http"
+	"strconv"
 
 	"github.com/traefik/genconf/dynamic"
-	"github.com/traefik/genconf/dynamic/tls"
+	// "github.com/traefik/genconf/dynamic/tls"
+	// "github.com/gorilla/websocket"
+	"golang.org/x/net/websocket"
+	"strings"
 )
 
 // Config the plugin configuration.
 type Config struct {
-	PollInterval string `json:"pollInterval,omitempty"`
+	Providers []ServiceProvider `json:"providers`
+	Methods []RPCMethod `json:"methods"`
+}
+
+type ServiceProvider struct {
+	URL string `json:"url"`
+	WSURL string `json:"wsurl"`
+	Methods []string `json:"methods"`
+	Archive bool `json:"archive"`
+	latestBlock int64
+}
+
+type newHeadsMessage struct {
+	Params struct {
+		Result struct {
+			Number string
+		} `json:"result"`
+	} `json:"params"`
+}
+
+func (sp *ServiceProvider) MonitorBlock(ctx context.Context, ch chan<- int64) {
+	if sp.WSURL == "" {
+		return
+	}
+	var conn *websocket.Conn
+	var err error
+	go func() {
+		<-ctx.Done()
+		conn.Close()
+	}()
+	go func () {
+		for {
+			if ctx.Err() != nil {
+				return
+			}
+			websocket.Dial(sp.WSURL, "", sp.URL)
+			// conn, _, err = websocket.DefaultDialer.Dial(sp.WSURL, nil)
+			if err != nil {
+				log.Printf("Failed to dial websockets for %v: %v", sp.WSURL, err.Error())
+			}
+			conn.Write([]byte(`{"id":0, "jsonrpc":"2.0", "method":"eth_subscribe", "params": ["newHeads"]}`))
+			msg := make([]byte, 2048)
+			_, err = conn.Read(msg) 
+			for {
+				_, err = conn.Read(msg) 
+				if err != nil {
+					log.Printf("Failed to read message on websockets for %v: %v", sp.WSURL, err.Error())
+					break
+				}
+				var nhm newHeadsMessage
+				json.Unmarshal(msg, &nhm)
+				num, err := strconv.ParseInt(nhm.Params.Result.Number, 0, 64)
+				if err != nil {
+					log.Printf("Error parsing number from head: %v - %v", nhm.Params.Result.Number, err.Error())
+				} else {
+					sp.latestBlock = num
+					ch <- num
+				}
+
+			}
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+}
+
+type RPCMethod struct {
+	Name string `json:"name"`
+	BlockSensitive bool `json:"blockSensitive"`
+	BlockSpecific bool `json:"blockSpecific"`
+	BlockArg bool `json:"blockArg"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
-		PollInterval: "5s", // 5 * time.Second
+		Methods: []RPCMethod{},
+		Providers: []ServiceProvider{},
 	}
 }
 
 // Provider a simple provider plugin.
 type Provider struct {
-	name         string
-	pollInterval time.Duration
+	name    string
+	methods []RPCMethod
+	serviceProvider map[string][]*ServiceProvider
+	blockNumCh <-chan int64
 
-	cancel func()
+	cancel  func()
 }
 
 // New creates a new Provider plugin.
 func New(ctx context.Context, config *Config, name string) (*Provider, error) {
-	pi, err := time.ParseDuration(config.PollInterval)
-	if err != nil {
-		return nil, err
+	sp := make(map[string][]*ServiceProvider)
+	ch := make(chan int64)
+	for _, provider := range config.Providers {
+		for _, m := range provider.Methods {
+			if _, ok := sp[m]; !ok {
+				sp[m] = []*ServiceProvider{}
+			}
+			sp[m] = append(sp[m], &provider)
+		}
+		provider.MonitorBlock(ctx, ch)
 	}
-
 	return &Provider{
 		name:         name,
-		pollInterval: pi,
+		methods: config.Methods,
+		serviceProvider: sp,
+		blockNumCh: ch,
 	}, nil
 }
 
 // Init the provider.
 func (p *Provider) Init() error {
-	if p.pollInterval <= 0 {
-		return fmt.Errorf("poll interval must be greater than 0")
-	}
-
 	return nil
 }
 
@@ -73,15 +154,15 @@ func (p *Provider) Provide(cfgChan chan<- json.Marshaler) error {
 }
 
 func (p *Provider) loadConfiguration(ctx context.Context, cfgChan chan<- json.Marshaler) {
-	ticker := time.NewTicker(p.pollInterval)
-	defer ticker.Stop()
-
+	var bn int64
 	for {
 		select {
-		case t := <-ticker.C:
-			configuration := generateConfiguration(t)
-
-			cfgChan <- &dynamic.JSONPayload{Configuration: configuration}
+		case block := <-p.blockNumCh:
+			if block > bn {
+				bn = block
+				configuration := p.generateConfiguration(bn)
+				cfgChan <- &dynamic.JSONPayload{Configuration: configuration}
+			}
 
 		case <-ctx.Done():
 			return
@@ -95,7 +176,7 @@ func (p *Provider) Stop() error {
 	return nil
 }
 
-func generateConfiguration(date time.Time) *dynamic.Configuration {
+func(p *Provider) generateConfiguration(bn int64) *dynamic.Configuration {
 	configuration := &dynamic.Configuration{
 		HTTP: &dynamic.HTTPConfiguration{
 			Routers:           make(map[string]*dynamic.Router),
@@ -103,53 +184,50 @@ func generateConfiguration(date time.Time) *dynamic.Configuration {
 			Services:          make(map[string]*dynamic.Service),
 			ServersTransports: make(map[string]*dynamic.ServersTransport),
 		},
-		TCP: &dynamic.TCPConfiguration{
-			Routers:  make(map[string]*dynamic.TCPRouter),
-			Services: make(map[string]*dynamic.TCPService),
-		},
-		TLS: &dynamic.TLSConfiguration{
-			Stores:  make(map[string]tls.Store),
-			Options: make(map[string]tls.Options),
-		},
-		UDP: &dynamic.UDPConfiguration{
-			Routers:  make(map[string]*dynamic.UDPRouter),
-			Services: make(map[string]*dynamic.UDPService),
-		},
 	}
 
-	configuration.HTTP.Routers["pp-route-01"] = &dynamic.Router{
+	configuration.HTTP.Routers["loopback"] = &dynamic.Router{
 		EntryPoints: []string{"web"},
-		Service:     "pp-service-01",
-		Rule:        "Host(`example.com`)",
+		Service:     "loopback",
+		Rule:        "Host(`/router`)",
 	}
 
-	configuration.HTTP.Services["pp-service-01"] = &dynamic.Service{
+	configuration.HTTP.Middlewares["rpcloopback"] = &dynamic.Middleware{
+		Plugin: make(map[string]dynamic.PluginConf),
+	}
+	configuration.HTTP.Middlewares["rpcloopback"].Plugin["rpcloopback"] = dynamic.PluginConf{}
+
+	configuration.HTTP.Services["loopback"] = &dynamic.Service{
 		LoadBalancer: &dynamic.ServersLoadBalancer{
 			Servers: []dynamic.Server{
 				{
-					URL: "http://localhost:9090",
+					URL: "http://localhost:8000",
 				},
 			},
 			PassHostHeader: boolPtr(true),
 		},
 	}
 
-	if date.Minute()%2 == 0 {
-		configuration.HTTP.Routers["pp-route-02"] = &dynamic.Router{
-			EntryPoints: []string{"web"},
-			Service:     "pp-service-02",
-			Rule:        "Host(`another.example.com`)",
+	for _, method := range p.methods {
+		servers := []dynamic.Server{}
+		for _, sp := range p.serviceProvider[method.Name] {
+			if sp.latestBlock >= bn {
+				servers = append(servers, dynamic.Server{
+					URL: sp.URL,
+				})
+			}
 		}
-
-		configuration.HTTP.Services["pp-service-02"] = &dynamic.Service{
+		configuration.HTTP.Services[method.Name] = &dynamic.Service{
 			LoadBalancer: &dynamic.ServersLoadBalancer{
-				Servers: []dynamic.Server{
-					{
-						URL: "http://localhost:9091",
-					},
-				},
-				PassHostHeader: boolPtr(true),
+				PassHostHeader: boolPtr(false),
+				Servers: servers,
 			},
+		}
+		path := "/" + strings.Join(strings.Split(method.Name, "_"), "/")
+		configuration.HTTP.Routers[method.Name] = &dynamic.Router{
+			EntryPoints: []string{"web"},
+			Service:     method.Name,
+			Rule:        fmt.Sprintf("PathPrefix(`%v`)", path),
 		}
 	}
 
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/traefik/pluginproviderdemo
 go 1.19
 
 require github.com/traefik/genconf v0.3.0
+
+require golang.org/x/net v0.17.0 // indirect
